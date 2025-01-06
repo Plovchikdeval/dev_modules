2e658389a7d161c471a417eb0f7ed7b32d08e126
Plovchikdeval <plov99502@gmail.com>
2025-01-06 14:12:50 +0500

    Update VoiceChat.py

--- VoiceChat.py
+++ VoiceChat.py
@@ -1,4 +1,4 @@
-__version__ = (3, 0, 0)
+__version__ = (3, 0, 1)
 
 # ---------------------------------------------------------------------------------
 #  /\_/\  🌐 This module was loaded through https://t.me/hikkamods_bot
