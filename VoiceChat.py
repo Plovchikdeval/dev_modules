8b24021964356751c2466232ef1f49e29790dbb4
Plovchikdeval <plov99502@gmail.com>
2025-01-20 05:43:54 +0500

    Update VoiceChat.py

--- VoiceChat.py
+++ VoiceChat.py
@@ -19,7 +19,7 @@ __version__ = (3, 0, 1)
 
 # meta pic: https://static.dan.tatar/voicechat_icon.png
 # meta banner: https://mods.hikariatama.ru/badges/voicechat.jpg
-# meta developer: @frost_shard & @devjmodules
+# meta developer: @kshmods
 # coded by : @hikarimods
 # scope: hikka_only
 # scope: hikka_min 1.3.0
