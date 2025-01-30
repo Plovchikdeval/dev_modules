8004810893593add55dbedebeafbb73615591174
Plovchikdeval <plov99502@gmail.com>
2025-01-30 20:02:02 +0500

    Update DevGPT.py

--- DevGPT.py
+++ DevGPT.py
@@ -1,5 +1,5 @@
-__version__ = (7, 0, 0)
-# change-log: ADDED NEW MODEL!!
+__version__ = (7, 0, 1)
+# change-log: SHOW TO DESAI.
 
 """
 888    d8P   .d8888b.  888    888     888b     d888  .d88888b.  8888888b.   .d8888b.  
